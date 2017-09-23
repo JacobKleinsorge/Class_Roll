e1aef0ff9cf9ff0f6744bb0a8d386195635bfa38
Jacob Kleinsorge <jacobkleinsorge@gmail.com>
2017-09-23 13:10:10 -0400

    Added welcome text and help text

--- ClassRoll/ClassRoll/Main.cpp
+++ ClassRoll/ClassRoll/Main.cpp
@@ -15,18 +15,23 @@ public:
 };
 
 int main(int argc, const char * argv[]) {
-
-
+	cout << "Welcome to our class roll system.";
+	helpText();
 
 	while (true) {
-
-
-
-
-
 
 	}
 
 
 
+}
+
+void helpText() {
+	cout << endl << "Commands:" << endl;
+	cout << "    - " << "Add" << "               // Adds student(s) manually or from a file" << endl;
+	cout << "    - " << "Delete" << "               // Deletes a student from the system" << endl;
+	cout << "    - " << "List" << "               // Lists the students by last name" << endl;
+	cout << "    - " << "Search" << "               // Search through students by name, ID, or email" << endl;
+	cout << "    - " << "Update" << "               // Update any of the student fields" << endl;
+	cout << "    - " << "Help" << "               // Lists these commands" << endl;
 }
