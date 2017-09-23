5a0f9802f87f109c1d485f45d3bfc12f17e8a5de
r1singphoenix <32224244+r1singphoenix@users.noreply.github.com>
2017-09-23 15:17:12 -0400

    Add listStudents(), add roll array

--- ClassRoll/ClassRoll/Main.cpp
+++ ClassRoll/ClassRoll/Main.cpp
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <string>
 
+#define CLASS_SIZE 50
+
 using namespace std;
 
 class student
@@ -16,6 +18,9 @@ public:
 };
 
 int main(int argc, const char * argv[]) {
+	int numStudents = 0;
+	student roll[CLASS_SIZE];
+	
 	cout << "Welcome to our class roll system.";
 	helpText();
 
@@ -35,4 +40,14 @@ void helpText() {
 	cout << "    - " << "Search" << "               // Search through students by name, ID, or email" << endl;
 	cout << "    - " << "Update" << "               // Update any of the student fields" << endl;
 	cout << "    - " << "Help" << "               // Lists these commands" << endl;
+}
+
+void listStudents(student* roll, int numStudents)
+{
+	for (int i = 0; i < numStudents; i++)
+	{
+		cout << roll[i].lastName << ", " << roll[i].firstName << endl;
+	}
+
+	return;
 }
