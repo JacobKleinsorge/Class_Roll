b06962571c540cee6037f76d6fc64ccb797a5934
Jacob Kleinsorge <jacobkleinsorge@gmail.com>
2017-09-23 18:13:16 -0400

    Update Main.cpp

--- ClassRoll/ClassRoll/Main.cpp
+++ ClassRoll/ClassRoll/Main.cpp
@@ -8,12 +8,7 @@
 #include <istream>
 
 #define CLASS_SIZE 50
-
 using namespace std;
-
-int addStudent(int numStudents);
-void listStudents();
-void helpText();
 
 class student
 {
@@ -27,49 +22,49 @@ public:
 	string email;
 };
 
-int main(int argc, const char * argv[]) {
+int addStudent(student* roll, int numStudents);
+int deleteStudent(student* roll, int numStudents);
+void listStudents(student* roll, int numStudents);
+void helpText();
+
+void main(int argc, const char * argv[]) {
 	int numStudents = 0;
 	student roll[CLASS_SIZE];
-	
-	cout << "Welcome to our class roll system.";
-	helpText();
-	
-	char input;
-	
-	while (true) {
-		cin >> input;
-		
-		switch (input) {
-			case 'a':
-				addStudent(numStudents);
-				break;
-			case 'd':
-				Delete();
-				break;
-			case 'l':
-				listStudents();
-				break;
-			case 's':
-				Search();
-				break;
-			case 'u':
-				Update();
-				break;
-			case 'h':
-				helpText();
-				break;
-			case 'q':
-				return;
-				break;
-			default:
-				cout << "Incorrect input, please use one of the following commands: ";
-				helpText();
 
+	cout << "Welcome to our class roll system.";
+	char input;
+
+	while (true) {
+		helpText();
+		cin >> input;
+
+		switch (input) {
+		case 'a':
+			numStudents = addStudent(roll, numStudents);
+			break;
+		case 'd':
+			numStudents = deleteStudent(roll, numStudents);
+			break;
+		case 'l':
+			listStudents(roll, numStudents);
+			break;
+		/*case 's':
+			Search();
+			break;
+		case 'u':
+			Update();
+			break;*/
+		case 'h':
+			helpText();
+			break;
+		case 'q':
+			return;
+			break;
+		default:
+			cout << endl << "Incorrect input, please use one of the following commands: " << endl;
+			helpText();
 		}
 	}
-
-
-
 }
 
 void helpText() {
@@ -85,6 +80,7 @@ void helpText() {
 
 void listStudents(student* roll, int numStudents)
 {
+	cout << endl;
 	for (int i = 0; i < numStudents; i++)
 	{
 		cout << roll[i].lastName << ", " << roll[i].firstName << endl;
@@ -93,18 +89,18 @@ void listStudents(student* roll, int numStudents)
 	return;
 }
 
-int addStudent(int numStudents) {
-	char input;
+int addStudent(student* roll, int numStudents) {
+	char input = ' ';
 	string strInput;
 
-	if (numStudents == 50) {
-		cout << "There are already the maximum number of students in the class, please delete one to add another.";
-		return;
+	if (numStudents == CLASS_SIZE) {
+		cout << "There are already the maximum number of students in the class, please delete one to add another." << endl;
+		return numStudents;
 	}
-	
+
 	cout << "You can add one student manually, or multiple from a text file." << endl;
 	cout << "Enter 'm' for manual input or 'f' for file input or 'q' for quit: ";
-	
+
 	while (input != 'q') {
 
 		cin >> input;
@@ -114,9 +110,9 @@ int addStudent(int numStudents) {
 
 		if (input == 'f') {
 			cout << endl << "Text file input selected." << endl;
-			cout << "Files should be formatted as: usfid last-name first-name email presentation-grade essay-grade project-grade ";
+			cout << "Files should be formatted as: usfid last-name first-name email presentation-grade essay-grade project-grade " << endl;
 			cout << "There should be one student per line." << endl;
-			cout << "** Note: There are a maximum number of fifty students in the class";
+			cout << "** Note: There are a maximum number of fifty students in the class" << endl;
 			cout << "Please enter the name of the file to read from (inlcuding the extension): ";
 			cin >> strInput;
 
@@ -135,7 +131,7 @@ int addStudent(int numStudents) {
 			}
 
 			string line;
-			while (getline(studentFile, line) && numStudents < 50) {
+			while (getline(studentFile, line) && numStudents < CLASS_SIZE) {
 				istringstream iss(line);
 				vector<string> results((istream_iterator<string>(iss)), istream_iterator<string>());
 
@@ -166,6 +162,8 @@ int addStudent(int numStudents) {
 			cin >> roll[numStudents].essGrade;
 			cout << "Enter the student's project grade: ";
 			cin >> roll[numStudents].projGrade;
+			
+			numStudents++;
 			return numStudents;
 		}
 		else {
@@ -173,3 +171,33 @@ int addStudent(int numStudents) {
 		}
 	}
 }
+
+int deleteStudent(student* roll, int numStudents) {
+	string input;
+	cout << "Enter the USF ID number of the student you wish to delete (or 'q' for quit): ";
+
+	while (true) {
+		cin >> input;
+
+		if (input == "q") {
+			return numStudents;
+		}
+		for (int i = 0; i < numStudents; i++) {
+			if (roll[i].usfid == stoi(input)) {
+				roll[i].usfid = 0;
+				roll[i].firstName = "";
+				roll[i].lastName = "";
+				roll[i].email = "";
+				roll[i].presGrade = 0;
+				roll[i].essGrade = 0;
+				roll[i].projGrade = 0;
+				--numStudents;
+				return numStudents;
+			}
+		}
+		cout << "Student ID not found, please try again (or 'q' to quit)";
+	}
+}
+
+
+
